dca24c82a22a114e1cb2145af7548cc71ed0e1f3
carl sednaoui <carlsednaoui@gmail.com>
2013-08-01 13:53:09 -0400

    Naming and spacing changes

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import httplib2
 import datetime
 
@@ -7,10 +6,11 @@ from oauth2client.file import Storage
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.tools import run
 
-from flask import Flask, render_template, make_response, request, Response, redirect
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 CLIENT_SECRETS = 'client_secrets.json'
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%S-04:00'
 
 # Set up a Flow object to be used for authentication.
 FLOW = flow_from_clientsecrets(CLIENT_SECRETS,
@@ -19,6 +19,7 @@ FLOW = flow_from_clientsecrets(CLIENT_SECRETS,
       'https://www.googleapis.com/auth/calendar.readonly',
     ],
     message="MISSING_CLIENT_SECRETS")
+
 
 def authenticate_google_calendar():
   # If the Credentials don't exist or are invalid, run through the native
@@ -37,21 +38,19 @@ def authenticate_google_calendar():
 
   return build('calendar', 'v3', http=http)
 
+
 def list_events():
   service = authenticate_google_calendar()
   upcoming_events = []
 
-  """
-  List events
-  """
   # Use current_time to pull events happening in the future
   # We're using EST (-4:00 hours)
   now = datetime.datetime.now()
-  current_time = now.strftime('%Y-%m-%dT%H:%M:%S-04:00')
+  current_time = now.strftime(TIME_FORMAT)
 
   # Use max_time to look at events happening in the next 2 weeks
   two_weeks_from_now = now + datetime.timedelta(days=14)
-  max_time = two_weeks_from_now.strftime('%Y-%m-%dT%H:%M:%S-04:00')
+  max_time = two_weeks_from_now.strftime(TIME_FORMAT)
   print max_time
 
   # Parameters definition
@@ -72,22 +71,27 @@ def list_events():
       break
   return upcoming_events
 
-def create_event(summary, name, tel, email, start_time):
+
+# check to make sure timeslot is available prior to scheduling new event
+def check_time_availability(start_time, end_time):
   service = authenticate_google_calendar()
-  
+
+
+def create_event(summary, name, tel, email, start_time):
   # Ensure necessary fields are passed
   if summary == '' or name == '' or tel == '' or email == '' or start_time == '':
     print "You're missing a field"
     return
 
-  """
-  Create new event
-  """
+  service = authenticate_google_calendar()
   full_description = 'Call %s at %s. Email: %s' % (name, tel, email)
 
-  formatted_start_time = datetime.datetime.strptime(start_time,'%Y-%m-%d %H:%M').strftime('%Y-%m-%dT%H:%M:%S-04:00')
+  # Format start time. Expected input = YYYY-MM-DD HH:MM
+  formatted_start_time = datetime.datetime.strptime(start_time,'%Y-%m-%d %H:%M').strftime(TIME_FORMAT)
+  
+  # Calculate end time by adding 30 minutes to start time.
   end_time = datetime.datetime.strptime(start_time,'%Y-%m-%d %H:%M') + datetime.timedelta(minutes=30)
-  formatted_end_time = end_time.strftime('%Y-%m-%dT%H:%M:%S-04:00')
+  formatted_end_time = end_time.strftime(TIME_FORMAT)
 
   event = {
     'summary': summary,
@@ -101,21 +105,25 @@ def create_event(summary, name, tel, email, start_time):
     }
   }
 
-  # Uncomment below to execute
-  created_event = service.events().insert(calendarId='primary', body=event).execute()
+  # Create the event
+  service.events().insert(calendarId='primary', body=event).execute()
+
 
 @app.route('/')
 def view_calendar():
   events = list_events()
   return render_template('schedule.html', event_list=events)
 
+
 @app.route('/new', methods=['POST'])
 def new_event():
-  # assert False  
-  created_event = create_event(request.form['summary'], request.form['name'], 
+  create_event(request.form['summary'], request.form['name'], 
     request.form['tel'], request.form['email'], request.form['start-time'])
   return redirect('/')
 
+
 if __name__ == '__main__':
-  app.debug = True # server will reload itself on code changes
+  # Enable server to reload on code changes
+  # You can also add '$ assert False' to debug
+  app.debug = True 
   app.run()
